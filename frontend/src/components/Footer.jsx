127f09e3735df30dd1cf5a89c208ec6d201e8215
rakesh <rakeshmanna762@gmail,com>
2024-09-29 22:50:18 +0530

    frontend:footer component designed

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import { ArrowBigUp, Facebook, Instagram, Mail, Search, Twitter, Youtube } from 'lucide-react';
 
 const Footer = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div className='h-72 w-full p-4 bg-black text-white flex flex-col gap-3 justify-between items-center'>
+            <div className='flex gap-3'>
+                <ArrowBigUp />
+                <p>BACK TO TOP</p>
+
+            </div>
+            {/* <div className='flex gap-4'>
+                <Facebook />
+                <Instagram />
+                <Twitter />
+                <Youtube />
+                <Mail />
+            </div> */}
+            <div>
+                <p>Made with ♥ by <a href="">Rakesh</a> </p>
+            </div>
+        </div>
+    )
 }
 
 export default Footer
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,12 +1,13 @@
+import Footer from "../components/Footer"
 import Header from "../components/Header"
 import MainContent from "../components/MainContent"
 
 const HomePage = () => {
     return (
-        <div className="min-h-screen w-full ">
+        <div className="min-h-full w-full ">
             <Header />
             <MainContent />
-
+            <Footer />
         </div>
     )
 }
