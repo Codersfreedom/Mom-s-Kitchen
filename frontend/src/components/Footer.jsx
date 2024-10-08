bfba7a5cc032000b6e98cbb20641296c50567079
rakesh <rakeshmanna762@gmail,com>
2024-10-08 20:15:35 +0530

    frontend:footer section modified

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,22 +1,30 @@
-import { ArrowBigUp, Facebook, Instagram, Mail, Search, Twitter, Youtube } from 'lucide-react';
+import { Button } from '@chakra-ui/react';
+import { ArrowBigUp, Facebook, Heart, Instagram, Mail, Twitter, Youtube } from 'lucide-react';
 
 const Footer = () => {
+    const handleScrollTop = () => {
+        globalThis.scrollTo({ top: 0, left: 0, behaviour: "smooth" })
+    }
     return (
         <div className='h-72 z-30 w-full p-4 bg-black text-white flex flex-col gap-3 justify-between items-center'>
-            <div className='flex gap-3'>
-                <ArrowBigUp />
-                <p>BACK TO TOP</p>
+            <div className='flex gap-3 items-center'>
+                <Button
+                leftIcon={<ArrowBigUp/>}
+                onClick={handleScrollTop}
+                >Scroll To Top</Button>
+
 
             </div>
-            {/* <div className='flex gap-4'>
+            <div className='flex gap-4'>
                 <Facebook />
-                <Instagram />
+                <a href="https://instagram.com/rakeshmanna717"><Instagram />
+                </a>
                 <Twitter />
                 <Youtube />
-                <Mail />
-            </div> */}
+                <a href="mailto:rakeshmanna762@gmail.com"><Mail /></a>
+            </div>
             <div>
-                <p>Made with ♥ by <a href="">Rakesh</a> </p>
+                <p className='flex items-center gap-2'>Made With {<Heart color='red' fill='red' />} By <a target='_blank' href="https://github.com/codersfreedom">Rakesh</a> </p>
             </div>
         </div>
     )
